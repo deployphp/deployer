52b7af8e0a3bc711204304f657ee096d115c6f37
Elfet <anton@elfet.ru>
2013-07-12 23:19:38 +0400

    Add compile class and command.

---
+++ src/Deployer/Utils/Compiler.php
@@ -0,0 +1,102 @@
+<?php
+/* (c) Anton Medvedev <anton@elfet.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Deployer\Utils;
+
+use Symfony\Component\Finder\Finder;
+
+class Compiler
+{
+    public function compile($fromDir, $pharFile = 'deployer.phar')
+    {
+        if (file_exists($pharFile)) {
+            unlink($pharFile);
+        }
+
+        $phar = new \Phar($pharFile, 0, 'deployer.phar');
+        $phar->setSignatureAlgorithm(\Phar::SHA1);
+
+        $phar->startBuffering();
+
+        $finder = new Finder();
+        $finder->files()
+            ->ignoreVCS(true)
+            ->name('*.php')
+            ->exclude('Tests')
+            ->exclude('bin')
+            ->notName('Compiler.php')
+            ->in($fromDir);
+
+        foreach ($finder as $file) {
+            $this->addFile($phar, $file);
+        }
+
+        $phar->setStub($this->getStub());
+
+        $phar->stopBuffering();
+
+        echo "Phar package compiled successful.\n";
+
+        unset($phar);
+    }
+
+    private function addFile(\Phar $phar, \SplFileInfo $file, $strip = true)
+    {
+        $path = str_replace(dirname(dirname(__DIR__)).DIRECTORY_SEPARATOR, '', $file->getRealPath());
+
+        $content = file_get_contents($file);
+        if ($strip) {
+            $content = $this->stripWhitespace($content);
+        }
+
+        $phar->addFromString($path, $content);
+    }
+
+    /**
+     * Removes whitespace from a PHP source string while preserving line numbers.
+     *
+     * @param  string $source A PHP string
+     * @return string The PHP string with the whitespace removed
+     */
+    private function stripWhitespace($source)
+    {
+        if (!function_exists('token_get_all')) {
+            return $source;
+        }
+
+        $output = '';
+        foreach (token_get_all($source) as $token) {
+            if (is_string($token)) {
+                $output .= $token;
+            } elseif (in_array($token[0], array(T_COMMENT, T_DOC_COMMENT))) {
+                $output .= str_repeat("\n", substr_count($token[1], "\n"));
+            } elseif (T_WHITESPACE === $token[0]) {
+                // reduce wide spaces
+                $whitespace = preg_replace('{[ \t]+}', ' ', $token[1]);
+                // normalize newlines to \n
+                $whitespace = preg_replace('{(?:\r\n|\r|\n)}', "\n", $whitespace);
+                // trim leading spaces
+                $whitespace = preg_replace('{\n +}', "\n", $whitespace);
+                $output .= $whitespace;
+            } else {
+                $output .= $token[1];
+            }
+        }
+
+        return $output;
+    }
+
+    private function getStub()
+    {
+        return <<<'EOF'
+<?php
+require __DIR__ . '/vendor/autoload.php';
+new Deployer\Tool();
+__HALT_COMPILER();
+EOF;
+    }
+}

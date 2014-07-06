39d38e30be66fcd2251122abe0dc64328a59a3a1
Elfet <anton@elfet.ru>
2014-07-06 14:02:23 +0400

    Add recipe include path and symfony.php.

---
+++ recipe/symfony.php
@@ -0,0 +1,8 @@
+<?php
+/* (c) Anton Medvedev <anton@elfet.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+ 

94e0463e43b777d87c15eba8ff66bb738f7feda7
jelte <jelte@marlon.be>
2014-08-05 00:51:13 +0200

    Throw exception when defining stage before servers

--- src/Stage/StageFactory.php
+++ src/Stage/StageFactory.php
@@ -8,20 +8,29 @@ use Deployer\Deployer;
 
 class StageFactory
 {
+
     /**
      * @param $name
      * @param array $servers
      * @param bool $default
      * @return array
+     * @throws \RuntimeException
      */
     public static function create($name, array $servers, $default = false)
     {
+        if ( count(Deployer::$servers) == 0 ) {
+            throw new \RuntimeException('Server should be defined before you define any stages.');
+        }
+
         // Automatically turn on multistage support when creating a stage
         Deployer::$multistage = true;
 
         // Make the server list for this stage
         $servers = array_combine($servers, $servers);
         array_walk($servers, function(&$value, $name) {
+            if ( !isset(Deployer::$servers[$name]) ) {
+                throw new \RuntimeException(sprintf('Server "%s" not found', $name));
+            }
             $value = Deployer::$servers[$name];
         });
 

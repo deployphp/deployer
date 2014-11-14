96192d6850bb9b5ecdff534dcc2ebd216893813d
gordalina <samuel.gordalina@gmail.com>
2014-11-14 15:26:55 +0000

    Add cachetool recipe

---
+++ recipe/cachetool.php
@@ -0,0 +1,40 @@
+<?php
+/* (c) Samuel Gordalina <samuel.gordalina@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Clear apc cache
+ */
+task('cachetool:clear:apc', function () {
+    $releasePath = env()->getReleasePath();
+    $options = get('cachetool', '');
+
+    cd($releasePath);
+    $hasCachetool = run("if [ -e $releasePath/cachetool.phar ]; then echo 'true'; fi");
+
+    if ('true' !== $hasCachetool) {
+        run("curl -sO http://gordalina.github.io/cachetool/downloads/cachetool.phar");
+    }
+
+    run("php cachetool.phar apc:cache:clear system {$options}");
+});
+
+/**
+ * Clear opcache cache
+ */
+task('cachetool:clear:opcache', function () {
+    $releasePath = env()->getReleasePath();
+    $options = get('cachetool', '');
+
+    cd($releasePath);
+    $hasCachetool = run("if [ -e $releasePath/cachetool.phar ]; then echo 'true'; fi");
+
+    if ('true' !== $hasCachetool) {
+        run("curl -sO http://gordalina.github.io/cachetool/downloads/cachetool.phar");
+    }
+
+    run("php cachetool.phar opcache:reset {$options}");
+});

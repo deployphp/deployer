242505866134aa13922c35bb14acf29264c4c0e3
Hubert Viktor <rpgmorpheus@gmail.com>
2015-05-21 09:07:41 +0200

    Added new tests to the testEnv() method according to the changes made in the Builder.

--- test/src/Server/BuilderTest.php
+++ test/src/Server/BuilderTest.php
@@ -112,7 +112,31 @@ class BuilderTest extends \PHPUnit_Framework_TestCase
     {
         $config = $this->getMockBuilder('Deployer\Server\Configuration')->disableOriginalConstructor()->getMock();
         $env = $this->getMock('Deployer\Server\Environment');
-        $env->expects($this->once())
+
+        // Configuring stubs
+        $config
+            ->method('getName')
+            ->will($this->returnValue('test-name'));
+        $config
+            ->method('getHost')
+            ->will($this->returnValue('test-host'));
+        $config
+            ->method('getPort')
+            ->will($this->returnValue(22));
+
+        // The Builder class should create the server env variable.
+        $env->expects($this->at(0))
+            ->method('set')
+            ->with('server', [
+                'name' => 'test-name',
+                'host' => 'test-host',
+                'port' => 22,
+            ])
+            ->will($this->returnSelf());
+
+        // The `env` method of the Builder class should internally call the
+        // Environment's `set` method.
+        $env->expects($this->at(1))
             ->method('set')
             ->with('name', 'value')
             ->will($this->returnSelf());
@@ -120,7 +144,7 @@ class BuilderTest extends \PHPUnit_Framework_TestCase
         $b = new Builder($config, $env);
         $b->env('name', 'value');
     }
-    
+
     public function testForwardAgent()
     {
         $config = $this->getMockBuilder('Deployer\Server\Configuration')->disableOriginalConstructor()->getMock();

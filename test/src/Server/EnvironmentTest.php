35971b4e71d2e94e0c318dea38140bd0ef15cead
Hubert Viktor <rpgmorpheus@gmail.com>
2015-05-21 09:41:44 +0200

    Added the possibility of 'protected' env parameters. (+test).

--- test/src/Server/EnvironmentTest.php
+++ test/src/Server/EnvironmentTest.php
@@ -4,7 +4,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
- 
+
 namespace Deployer\Server;
 
 class EnvironmentTest extends \PHPUnit_Framework_TestCase
@@ -30,7 +30,7 @@ class EnvironmentTest extends \PHPUnit_Framework_TestCase
         $env->set('string', 'value');
         $env->set('array', [1, 'two']);
         $env->set('parse', 'is {{int}}');
-        
+
         $this->assertEquals(42, $env->get('int'));
         $this->assertEquals('value', $env->get('string'));
         $this->assertEquals([1, 'two'], $env->get('array'));
@@ -44,5 +44,15 @@ class EnvironmentTest extends \PHPUnit_Framework_TestCase
 
         $this->setExpectedException('RuntimeException', 'Environment parameter `so` does not exists.');
         $env->get('so');
+    }
+
+    public function testProtectedParameters()
+    {
+        $env = new Environment();
+
+        $env->set('protected', 'protected-value', true);
+
+        $this->setExpectedException('\RuntimeException', 'The environment parameter `protected` has already been set, and is protected from changes.');
+        $env->set('protected', 'some-other-value');
     }
 }

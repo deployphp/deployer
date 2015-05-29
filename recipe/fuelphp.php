ff2fbb62a0bf5af9eaa0d4205408281dd9047179
kenjis <kenji.uui@gmail.com>
2015-05-29 18:39:15 +0900

    Add FuelPHP Recipe

---
+++ recipe/fuelphp.php
@@ -0,0 +1,28 @@
+<?php
+/* (c) Anton Medvedev <anton@elfet.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once __DIR__ . '/common.php';
+
+// FuelPHP 1.x shared dirs
+set('shared_dirs', [
+    'fuel/app/cache', 'fuel/app/logs',
+]);
+
+/**
+ * Main task
+ */
+task('deploy', [
+    'deploy:prepare',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:vendors',
+    'deploy:shared',
+    'deploy:symlink',
+    'cleanup',
+])->desc('Deploy your project');
+
+after('deploy', 'success');

49a8927d75fd609f473b90ba8433a858e8199123
Felix Nehrke <felix@nehrke.info>
2016-03-11 03:44:48 +0100

    Clean ugly hack in output of an executing task

--- src/Executor/Informer.php
+++ src/Executor/Informer.php
@@ -31,14 +31,7 @@ class Informer
     public function startTask($taskName)
     {
         if ($this->output->getVerbosity() >= OutputInterface::VERBOSITY_NORMAL) {
-            if ($this->output->getVerbosity() == OutputInterface::VERBOSITY_NORMAL) {
-                $this->output->write("  ");
-            } else {
-                $this->output->write("➤ ");
-            }
-
-            $this->output->writeln("Executing task $taskName");
-
+            $this->output->writeln("➤ Executing task $taskName");
             $this->output->setWasWritten(false);
         }
     }

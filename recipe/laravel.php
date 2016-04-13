114395ee00614cd1256e3c05dc10ffd7a2532d9d
oanhnn <oanhnn.bk@gmail.com>
2016-04-13 09:54:44 +0700

    Add helper tasks for laravel recipe

--- recipe/laravel.php
+++ recipe/laravel.php
@@ -23,6 +23,38 @@ set('shared_files', ['.env']);
 // Laravel writable dirs
 set('writable_dirs', ['bootstrap/cache', 'storage']);
 
+task('deploy:migrate', function () {
+    $output = run('{{bin/php}} {{deploy_path}}/current/artisan migrate --force');
+    writeln('<info>' . $output . '</info>');
+})->desc('Execute artisan migrate');
+
+task('deploy:migrate_rollback', function () {
+    $output = run('{{bin/php}} {{deploy_path}}/current/artisan migrate:rollback --force');
+    writeln('<info>' . $output . '</info>');
+})->desc('Execute artisan migrate:rollback');
+
+task('deploy:migrate_status', function () {
+    $output = run('{{bin/php}} {{deploy_path}}/current/artisan migrate:status');
+    writeln('<info>' . $output . '</info>');
+})->desc('Show status of migration');
+
+/**
+ * Task deploy:public_disk support the public disk.
+ * To run this task automatically, please add below line to your deploy.php file
+ * <code>after('deploy:symlink', 'deploy:public_disk');</code>
+ * @see https://laravel.com/docs/5.2/filesystem#configuration
+ */
+task('deploy:public_disk', function () {
+    // Remove from source.
+    run('if [ -d $(echo {{release_path}}/public/storage) ]; then rm -rf {{release_path}}/public/storage; fi');
+
+    // Create shared dir if it does not exist.
+    run('mkdir -p {{deploy_path}}/shared/storage/app/public');
+
+    // Symlink shared dir to release dir
+    run('ln -nfs {{deploy_path}}/shared/storage/app/public {{release_path}}/public/storage');
+})->desc('Make symlink for public disk');
+
 /**
  * Main task
  */

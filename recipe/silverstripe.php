a82a40c2c0f7eb08b9773e1111bf517481e27c2e
Corey Sewell <corey@sewell.net.nz>
2016-06-27 04:25:23 +1200

    Add support for Silverstripe CMS (#709)
    * Add support for Silverstripe CMS
    
    * Fix typo

---
+++ recipe/silverstripe.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once __DIR__ . '/common.php';
+
+/**
+ * Silverstripe configuration
+ */
+
+// Silverstripe shared dirs
+set('shared_dirs', [
+    'assets'
+]);
+
+// Silverstripe writable dirs
+set('writable_dirs', ['assets']);
+
+/**
+ * Helper tasks
+ */
+task('silverstripe:build', function () {
+    return run('{{bin/php}} {{release_path}}/framework/cli-script.php /dev/build');
+})->desc('Run /dev/build');
+
+task('silverstripe:buildflush', function () {
+    return run('{{bin/php}} {{release_path}}/framework/cli-script.php /dev/build flush=all');
+})->desc('Run /dev/build?flush=all');
+
+/**
+ * Main task
+ */
+task('deploy', [
+    'deploy:prepare',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:vendors',
+    'deploy:shared',
+    'deploy:writable',
+    'silverstripe:buildflush',
+    'deploy:symlink',
+    'cleanup',
+])->desc('Deploy your project');
+
+after('deploy', 'success');

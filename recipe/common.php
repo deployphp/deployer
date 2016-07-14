fc94f224a89366e120b63cb37e0c4e006f24baaf
Alwin Garside <alwin@myparcel.nl>
2016-07-14 15:24:49 +0200

    Properly rollback to previous release
    Properly rollback to _previous_ release, and not just $releases[1], so you can rollback even if current release is not the newest in the releases folder.

--- recipe/common.php
+++ recipe/common.php
@@ -78,19 +78,26 @@ option('revision', null, \Symfony\Component\Console\Input\InputOption::VALUE_OPT
  * Rollback to previous release.
  */
 task('rollback', function () {
-    $releases = env('releases_list');
+    $releases    = env('releases_list');
+    $currentPath = env('current');
+    $current     = basename($currentPath);
 
-    if (isset($releases[1])) {
-        $releaseDir = "{{deploy_path}}/releases/{$releases[1]}";
+    $n = 0;
+    while (isset($releases[$n]) && $releases[$n] >= $current) {
+        $n++;
+    }
+
+    if (isset($releases[$n])) {
+        $releaseDir = "{{deploy_path}}/releases/{$releases[$n]}";
 
         // Symlink to old release.
         run("cd {{deploy_path}} && ln -nfs $releaseDir current");
 
         // Remove release
-        run("rm -rf {{deploy_path}}/releases/{$releases[0]}");
+        run("rm -rf {$currentPath}");
 
         if (isVerbose()) {
-            writeln("Rollback to `{$releases[1]}` release was successful.");
+            writeln("Rollback to `{$releases[$n]}` release was successful.");
         }
     } else {
         writeln("<comment>No more releases you can revert to.</comment>");

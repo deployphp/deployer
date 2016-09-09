f9ba9959e4ad2a2995093564ac89ce7843b2d63d
Oscar Recio <oscar.recio@interactiv4.com>
2016-09-09 22:20:42 +0200

    Added new tasks to Magento Recipes and support Enterprise Edition

--- recipe/magento.php
+++ recipe/magento.php
@@ -24,21 +24,71 @@ set('writable_dirs', ['var', 'media']);
 /**
  * Clear cache
  */
-task('deploy:cache:clear', function () {
-    run("cd {{release_path}} && php -r \"require_once 'app/Mage.php'; umask(0); Mage::app()->cleanCache();\"");
+task('mage:cc', function () {
+    run("cd {{deploy_path}}/current && {{bin/php}} -r \"require_once 'app/Mage.php'; umask(0); Mage::app()->cleanCache();\"");
 })->desc('Clear cache');
+
+/**
+ * Clear Cache Static files
+ */
+task('mage:cc:static', function (){
+    run("cd {{deploy_path}}/current && rm -rf media/css/*.css && rm -rf media/js/*.js && rm -rf media/css_secure/*.css");
+})->desc('Clear cache images');
+
+/**
+ * Enter in Maintenance Mode Magento
+ */
+task('mage:disable', function (){
+    run("cd {{deploy_path}}/current && touch maintenance.flag");
+})->desc('Enable Maintenance Mode');
+
+/**
+ * Disable Maintenance Mode Magento
+ */
+task('mage:enable', function (){
+    run("cd {{deploy_path}}/current && rm -rf maintenance.flag");
+})->desc('Disable Maintenance Mode');
+
+/**
+ * Reindex all index of Magento
+ */
+task('mage:indexer',function(){
+    run("cd {{deploy_path}}/current/shell && {{bin/php}} -f indexer.php -- reindexall");
+})->desc('Run Indexer');
+
+/**
+ * Delete Locks of indexers
+ */
+task('mage:unlock',function(){
+    run("cd {{deploy_path}}/current && rm -rf var/locks/*");
+})->desc('Delete locks');
+
+/**
+ * Clean Magento Logs
+ */
+task('mage:clean_log',function(){
+    run("cd {{deploy_path}}/current/shell && {{bin/php}} -f log.php -- clean");
+})->desc('Clean Log');
+
 
 /**
  * Remove files that can be used to compromise Magento
  */
 task('deploy:clear_version', function () {
-    run("rm -f {{release_path}}/LICENSE.html");
-    run("rm -f {{release_path}}/LICENSE.txt");
-    run("rm -f {{release_path}}/LICENSE_AFL.txt");
-    run("rm -f {{release_path}}/RELEASE_NOTES.txt");
+    run("rm -f {{deploy_path}}/current/LICENSE.html");
+    run("rm -f {{deploy_path}}/current/LICENSE.txt");
+    run("rm -f {{deploy_path}}/current/LICENSE_AFL.txt");
+    run("rm -f {{deploy_path}}/current/RELEASE_NOTES.txt");
+    /**
+     * Enterprise Support
+     */
+    run("rm -f {{deploy_path}}/current/LICENSE_EE.html");
+    run("rm -f {{deploy_path}}/current/LICENSE_EE.txt");
 })->setPrivate();
 
 after('deploy:update_code', 'deploy:clear_version');
+
+before('mage:indexer','mage:unlock');
 
 
 /**
@@ -50,7 +100,7 @@ task('deploy', [
     'deploy:update_code',
     'deploy:shared',
     'deploy:writable',
-    'deploy:cache:clear',
+    'mage:cc',
     'deploy:symlink',
     'cleanup',
 ])->desc('Deploy your project');

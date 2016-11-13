11baf6c1a19ac4bed299c43cf3cf891348fc140c
Ioannis Kappas <ikappas@devworks.gr>
2016-11-13 15:05:57 +0200

    Switch to \Deployer\Util\Composer for composer auth

--- src/Server/Builder.php
+++ src/Server/Builder.php
@@ -10,6 +10,7 @@ namespace Deployer\Server;
 use Deployer\Builder\BuilderInterface;
 use Deployer\Server\Password\AskPasswordGetter;
 use Deployer\Server\Password\PasswordGetterInterface;
+use Deployer\Util\Composer;
 
 /**
  * Build server configuration
@@ -181,94 +182,9 @@ class Builder implements BuilderInterface
      */
     public function forwardComposerAuth()
     {
-        $this->config->setComposerAuth($this->getComposerAuth());
+        $this->config->setComposerAuth(Composer::getComposerAuth());
 
         return $this;
-    }
-
-
-    protected static function getComposerAuth()
-    {
-        $composerAuth = '';
-
-        $environment = getenv('COMPOSER_AUTH');
-        if ($environment) {
-            $composerAuth = json_decode($environment);
-        }
-
-        if (empty($composerAuth)) {
-            $filename = self::getComposerHomeDir() . DIRECTORY_SEPARATOR . 'auth.json';
-            if (file_exists(realpath($filename))) {
-                $composerAuth = json_decode(file_get_contents($filename));
-            }
-        }
-
-        if (!empty($composerAuth)) {
-            return json_encode($composerAuth);
-        }
-        return '';
-    }
-
-    /**
-     * @throws \RuntimeException
-     * @return string
-     */
-    protected static function getComposerHomeDir()
-    {
-        $home = getenv('COMPOSER_HOME');
-        if ($home) {
-            return $home;
-        }
-
-        if (defined('PHP_WINDOWS_VERSION_BUILD')) {
-            if (!getenv('APPDATA')) {
-                throw new \RuntimeException('The APPDATA or COMPOSER_HOME environment variable must be set for composer to run correctly');
-            }
-
-            return rtrim(strtr(getenv('APPDATA'), '\\', '/'), '/') . '/Composer';
-        }
-
-        $userDir = self::getUserDir();
-        if (is_dir($userDir . '/.composer')) {
-            return $userDir . '/.composer';
-        }
-
-        if (self::useXdg()) {
-            // XDG Base Directory Specifications
-            $xdgConfig = getenv('XDG_CONFIG_HOME') ?: $userDir . '/.config';
-
-            return $xdgConfig . '/composer';
-        }
-
-        return $userDir . '/.composer';
-    }
-
-    /**
-     * @return bool
-     */
-    private static function useXdg()
-    {
-        foreach (array_keys($_SERVER) as $key) {
-            if (substr($key, 0, 4) === 'XDG_') {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    /**
-     * @throws \RuntimeException
-     * @return string
-     */
-    private static function getUserDir()
-    {
-        $home = getenv('HOME');
-        if (!$home) {
-            throw new \RuntimeException('The HOME or COMPOSER_HOME environment variable must be set for composer to run correctly');
-        }
-
-        return rtrim(strtr($home, '\\', '/'), '/');
     }
 
     /**

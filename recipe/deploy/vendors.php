a4077777e135db29f63d32a3bcc81c168573f5ae
Yisrael Dov Lebow <lebow@lebowtech.com>
2017-01-30 19:28:12 +0200

    Use the old vendors directory for composer

--- recipe/deploy/vendors.php
+++ recipe/deploy/vendors.php
@@ -9,5 +9,11 @@ namespace Deployer;
 
 desc('Installing vendors');
 task('deploy:vendors', function () {
+    $releases = get('releases_list');
+    if (isset($releases[1])) {
+        if (run("if [ -d {{deploy_path}}/releases/{$releases[1]}/vendor ]; then echo 'true'; fi")->toBool()) {
+            run("cp --recursive {{deploy_path}}/releases/{$releases[1]}/vendor {{release_path}}");
+        }
+    }
     run('cd {{release_path}} && {{env_vars}} {{bin/composer}} {{composer_options}}');
 });

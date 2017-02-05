a7a3fa176eeb0965eb77c48f0c045eb735390402
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2017-02-05 06:45:58 +0100

    Improve the WordPress recipe
    * Configure files commonly shared across deploys (`wp-config.php` and the uploads folder)
    * Run Composer (many WordPress sites use Composer with WPackagist)
    * chown file to the web server owner. (Might be platform-dependent, but this is a good default)

--- recipe/wordpress.php
+++ recipe/wordpress.php
@@ -9,15 +9,27 @@ namespace Deployer;
 
 require_once __DIR__ . '/common.php';
 
+set('shared_files', ['wp-config.php']);
+set('shared_dirs', ['wp-content/uploads']);
+
 /**
- * Main task
+ * Chown files to correct web server user
+ * for your OS, so uploads work.
  */
+task('deploy:chown', function () {
+  run('chown -R www-data:www-data ' . env('deploy_path'));
+});
+
 task('deploy', [
     'deploy:prepare',
     'deploy:lock',
     'deploy:release',
     'deploy:update_code',
+    'deploy:shared',
+    'deploy:vendors', 
+    'deploy:writable',
     'deploy:symlink',
+    'deploy:chown',
     'deploy:unlock',
     'cleanup',
 ])->desc('Deploy your project');

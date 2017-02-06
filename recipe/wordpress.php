0a055bc975e59bb1dd2ed3c026fc99e969befb76
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2017-02-06 06:01:23 +0100

    Improve the WordPress recipe (#997)
    * Improve the WordPress recipe 
    
    * Configure files commonly shared across deploys (`wp-config.php` and the uploads folder)
    * Run Composer (many WordPress sites use Composer with WPackagist)
    * chown file to the web server owner. (Might be platform-dependent, but this is a good default)
    
    * Fix whitespace
    
    * Fix whitespace, pt. 2
    
    * Use `writable_dirs` option
    
    * Remove chown task

--- recipe/wordpress.php
+++ recipe/wordpress.php
@@ -9,14 +9,18 @@ namespace Deployer;
 
 require_once __DIR__ . '/common.php';
 
-/**
- * Main task
- */
+set('shared_files', ['wp-config.php']);
+set('shared_dirs', ['wp-content/uploads']);
+set('writable_dirs', ['wp-content/uploads']);
+
 task('deploy', [
     'deploy:prepare',
     'deploy:lock',
     'deploy:release',
     'deploy:update_code',
+    'deploy:shared',
+    'deploy:vendors',
+    'deploy:writable',
     'deploy:symlink',
     'deploy:unlock',
     'cleanup',

1e152358c511c6ff13c935527cfc1720d4342da4
Mathias Brodala <mbrodala@pagemachine.de>
2017-02-07 16:47:26 +0100

    Add a few comments

--- src/Type/ArrayType.php
+++ src/Type/ArrayType.php
@@ -14,22 +14,33 @@ namespace Deployer\Type;
 class ArrayType
 {
     /**
+     * Recursively merge two arrays with a specific behavior:
+     *
+     * 1. scalar values are overridden
+     * 2. array values are extended uniquely if all keys are numeric
+     * 3. all other array values are merged
+     *
      * @param array $a
      * @param array $b
      * @return array
+     * @see http://stackoverflow.com/a/36366886/6812729
      */
     public static function merge(array $original, array $override)
     {
         foreach ($override as $key => $value) {
             if (isset($original[$key])) {
                 if (!is_array($original[$key])) {
+                    // Override scalar value
                     $original[$key] = $value;
                 } elseif (array_keys($original[$key]) === range(0, count($original[$key]) - 1)) {
+                    // Uniquely append to array with numeric keys
                     $original[$key] = array_unique(array_merge($original[$key], $value));
                 } else {
+                    // Merge all other arrays
                     $original[$key] = ArrayType::merge($original[$key], $value);
                 }
             } else {
+                // Simply add new key/value
                 $original[$key] = $value;
             }
         }

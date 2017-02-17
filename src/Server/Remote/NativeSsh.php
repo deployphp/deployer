a6ce58f0cf89471e7eeb7a3dbb774c621b7d6fc2
ahfeel <ahfeel@gmail.com>
2017-02-17 11:51:05 +0100

    * Allow using PEM files with native ssh.

--- src/Server/Remote/NativeSsh.php
+++ src/Server/Remote/NativeSsh.php
@@ -71,6 +71,8 @@ class NativeSsh implements ServerInterface
 
         if ($serverConfig->getPrivateKey()) {
             $sshOptions[] = '-i ' . escapeshellarg($serverConfig->getPrivateKey());
+        } else if ($serverConfig->getPemFile()) {
+            $sshOptions[] = '-i ' . escapeshellarg($serverConfig->getPemFile());
         }
 
         if ($serverConfig->getPty()) {
@@ -246,6 +248,8 @@ class NativeSsh implements ServerInterface
         }
         if ($serverConfig->getPrivateKey()) {
             $sshOptions[] = '-i ' . escapeshellarg($serverConfig->getPrivateKey());
+        } else if ($serverConfig->getPemFile()) {
+            $sshOptions[] = '-i ' . escapeshellarg($serverConfig->getPemFile());
         }
         $sshOptions = array_merge($sshOptions, $this->getMultiplexingSshOptions());
 

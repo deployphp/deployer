8ef68422a216abd927ae50f6be14f853808db252
Anton Medvedev <anton@medv.io>
2017-02-26 17:30:59 +0700

    Add project hash to stats

--- src/Deployer.php
+++ src/Deployer.php
@@ -293,6 +293,7 @@ class Deployer extends Container
         $stats = [
             'status' => 'success',
             'command_name' => $commandEvent->getCommand()->getName(),
+            'project_hash' => empty($this->config['repository']) ? null : sha1($this->config['repository']),
             'servers_count' => $this->servers->count(),
             'deployer_version' => $this->getConsole()->getVersion(),
             'deployer_phar' => $this->getConsole()->isPharArchive(),

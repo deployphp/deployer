0743ae3f8bfcf09b89a3576b0d74f6054cab13db
Anton Medvedev <anton@medv.io>
2017-03-22 23:29:26 +0700

    Fix docblock

--- src/Deployer.php
+++ src/Deployer.php
@@ -33,7 +33,7 @@ use function Deployer\Support\array_merge_alternate;
 /**
  * Deployer class represents DI container for configuring
  *
- * @property Console console
+ * @property Application console
  * @property Task\TaskCollection|Task\Task[] tasks
  * @property Host\HostCollection|Collection|Host\Host[] hosts
  * @property Collection config

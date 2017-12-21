9d1665ba5e267eb6f486476ecd4ef7d19043bcd1
Markus Staab <maggus.staab@googlemail.com>
2017-12-21 08:59:38 +0100

    print the task description when in debug mode

--- src/Console/Output/Informer.php
+++ src/Console/Output/Informer.php
@@ -38,6 +38,11 @@ class Informer
             !$task->isShallow()
         ) {
             $this->output->writeln("➤ Executing task <info>{$task->getName()}</info>");
+
+            if ($this->output->isDebug() && $task->getDescription()) {
+                $this->output->writeln("  ".$task->getDescription());
+            }
+
             $this->output->setWasWritten(false);
         }
     }

a103fc1d293206bf5fcb9aabef1f22f505abe433
Markus Staab <markus.staab@redaxo.de>
2017-12-28 09:42:27 +0100

    log symfony-console handled exceptions into logfile

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -15,6 +15,7 @@ use Symfony\Component\Console\Input\InputDefinition;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Deployer\Deployer;
 
 class Application extends Console
 {
@@ -162,4 +163,16 @@ class Application extends Console
     {
         $this->after = $callable;
     }
+
+    public function renderException(\Exception $e, OutputInterface $output)
+    {
+        parent::renderException($e, $output);
+
+        $deployer = Deployer::get();
+        if ($deployer) {
+            $deployer->logger->log($e->getMessage());
+            $deployer->logger->log($e->getTraceAsString());
+        }
+    }
+
 }

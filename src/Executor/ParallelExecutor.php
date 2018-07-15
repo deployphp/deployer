90872d351e5737866587d6e73c5712da5d9ab65b
Anton Medvedev <anton@medv.io>
2018-07-16 00:01:28 +0300

    Update ParallelExecutor.php

--- src/Executor/ParallelExecutor.php
+++ src/Executor/ParallelExecutor.php
@@ -145,7 +145,7 @@ class ParallelExecutor implements ExecutorInterface
         $this->startProcesses($processes);
 
         while ($this->areRunning($processes)) {
-            $this->gatherOutput($processes, $callback);            
+            $this->gatherOutput($processes, $callback);
             usleep(1000);
         }
         $this->gatherOutput($processes, $callback);

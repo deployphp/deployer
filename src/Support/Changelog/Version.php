2ad907e14d8b626bc3250ac1f0b1ba00cb8ab778
Jeffrey Cafferata <jeffrey@jcid.nl>
2018-11-11 03:00:08 +0100

    Code style fixes (#1762)
    * Update missing argument in PHPDoc.
    
    * Splits the IF-statement/workflow(s) for readability
    
    * Replace double quotes to single quotes.
    
    * Replaced alias functions by original functions.
    
    * Define return types.
    
    * Type casting can be used
    
    * Non-optimal regular expression, \d is a subset of \w.

--- src/Support/Changelog/Version.php
+++ src/Support/Changelog/Version.php
@@ -45,24 +45,24 @@ class Version
             return "- $item";
         };
 
-        $added = "";
-        $changed = "";
-        $fixed = "";
-        $removed = "";
+        $added = '';
+        $changed = '';
+        $fixed = '';
+        $removed = '';
         if (!empty($this->added)) {
-            $added = join("\n", array_map($f, $this->added));
+            $added = implode("\n", array_map($f, $this->added));
             $added = "### Added\n$added\n\n";
         }
         if (!empty($this->changed)) {
-            $changed = join("\n", array_map($f, $this->changed));
+            $changed = implode("\n", array_map($f, $this->changed));
             $changed = "### Changed\n$changed\n\n";
         }
         if (!empty($this->fixed)) {
-            $fixed = join("\n", array_map($f, $this->fixed));
+            $fixed = implode("\n", array_map($f, $this->fixed));
             $fixed = "### Fixed\n$fixed\n\n";
         }
         if (!empty($this->removed)) {
-            $removed = join("\n", array_map($f, $this->removed));
+            $removed = implode("\n", array_map($f, $this->removed));
             $removed = "### Removed\n$removed\n\n";
         }
 

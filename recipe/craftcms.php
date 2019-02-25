95a4bdbf869315b01ff208c8a4638d0e55c82759
Joren Van Hee <jorenvanhee@gmail.com>
2019-02-25 17:30:19 +0100

    Add craft command tasks useful for deployment

--- recipe/craftcms.php
+++ recipe/craftcms.php
@@ -24,6 +24,26 @@ set('writable_dirs', [
     'web/cpresources',
 ]);
 
+desc('Execute craft migrate/all');
+task('craft:migrate:all', function () {
+    run('{{bin/php}} {{release_path}}/craft migrate/all');
+});
+
+desc('Execute craft migrate/up');
+task('craft:migrate:up', function () {
+    run('{{bin/php}} {{release_path}}/craft migrate/up');
+});
+
+desc('Execute craft project-config/sync');
+task('craft:project_config:sync', function () {
+    run('{{bin/php}} {{release_path}}/craft project-config/sync');
+});
+
+desc('Execute craft cache/flush-all');
+task('craft:cache:flush_all', function () {
+    run('{{bin/php}} {{release_path}}/craft cache/flush-all');
+});
+
 task('deploy', [
     'deploy:info',
     'deploy:prepare',

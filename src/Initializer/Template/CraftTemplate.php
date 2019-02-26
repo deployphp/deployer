e6b2c0e1b7b530b9d74627e8ce3758077a1b5001
Joren Van Hee <jorenvanhee@gmail.com>
2019-02-26 10:22:17 +0100

    Add migrations and project-config sync to template

--- src/Initializer/Template/CraftTemplate.php
+++ src/Initializer/Template/CraftTemplate.php
@@ -16,4 +16,16 @@ class CraftTemplate extends FrameworkTemplate
     {
         return 'craftcms';
     }
+
+    protected function getExtraContent()
+    {
+        return <<<PHP
+
+// Run migrations and sync project config before symlink new release.
+// Craft should already be installed for this to work.
+// before('deploy:symlink', 'craft:project_config:sync');
+// before('deploy:symlink', 'craft:migrate:all');
+
+PHP;
+    }
 }

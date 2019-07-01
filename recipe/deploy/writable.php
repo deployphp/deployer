99082bc91394474027de92486477478a883c67b5
Jörn Wagner <joern.wagner@explicatis.com>
2019-07-01 13:34:36 +0200

    Escape dollar sign in shell command to be used in awk (#1844)

--- recipe/deploy/writable.php
+++ recipe/deploy/writable.php
@@ -24,7 +24,7 @@ task('deploy:writable', function () {
 
     if ($httpUser === false && ! in_array($mode, ['chgrp', 'chmod'], true)) {
         // Attempt to detect http user in process list.
-        $httpUserCandidates = explode("\n", run("ps axo comm,user | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | sort | awk '{print $NF}' | uniq"));
+        $httpUserCandidates = explode("\n", run("ps axo comm,user | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | sort | awk '{print \$NF}' | uniq"));
         if (count($httpUserCandidates) === 1) {
             $httpUser = $httpUserCandidates[0];
         }

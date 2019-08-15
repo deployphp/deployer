c90af0c86e4ffb29616cb5298368fe62b0f76e7d
Leo Feyer <github@contao.org>
2019-08-15 16:59:06 +0200

    Add a configuration file for Contao

---
+++ recipe/contao.php
@@ -0,0 +1,18 @@
+<?php
+/* (c) Anton Medvedev <anton@medv.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Deployer;
+
+require __DIR__.'/symfony4.php';
+
+set('bin/console', function () {
+    return parse('{{bin/php}} {{release_path}}/vendor/bin/contao-console --no-interaction');
+});
+
+set('console_options', function () {
+    return '--no-interaction --env={{symfony_env}}';
+});

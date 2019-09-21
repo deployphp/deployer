deployphp/deployer

commit 62db938884cb0cc43e85af67a300bcf9f58807dd

Update symfony4.php (#1907)
The .env and .env.<environment> files should be committed to the shared repository because they are the same for all developers and machines. However, the env files ending in .local (.env.local and .env.<environment>.local) should not be committed because only you will use them. In fact, the .gitignore file that comes with Symfony prevents them from being committed.

https://symfony.com/doc/current/configuration.html#managing-multiple-env-files

recipe/symfony4.php
@@ -10,7 +10,7 @@ namespace Deployer;
 require_once __DIR__ . '/common.php';
 
 set('shared_dirs', ['var/log', 'var/sessions']);
-set('shared_files', ['.env.local']);
+set('shared_files', ['.env']);
 set('writable_dirs', ['var']);
 set('migrations_config', '');
 

commit fd25ec80b3c67f7a70987a40a69bd0ee73dd702c

Update symfony4 recipe
Fix wrong change introduced in https://github.com/deployphp/deployer/commit/62db938884cb0cc43e85af67a300bcf9f58807dd

`.env` file should not be shared because it's release-specific. On the other hand, `.env.local` and `.env.local.php` files are specific to deployment environment and should be shared between releases.

See for details: https://github.com/deployphp/deployer/pull/1907#issuecomment-533779139

recipe/symfony4.php
@@ -10,7 +10,7 @@ namespace Deployer;
 require_once __DIR__ . '/common.php';
 
 set('shared_dirs', ['var/log', 'var/sessions']);
-set('shared_files', ['.env']);
+set('shared_files', ['.env.local.php', '.env.local']);
 set('writable_dirs', ['var']);
 set('migrations_config', '');
 
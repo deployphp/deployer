377adf977aafd44cbf641da0b2d06f4e78c7f72a
Vladimir Pak <TheRatW@gmail.com>
2020-01-30 11:08:11 +0200

    Speedup deploy:clear_paths (#1992)
    * Update clear_paths.php
    
    one run much faster than several
    
    * Update CHANGELOG.md

--- recipe/deploy/clear_paths.php
+++ recipe/deploy/clear_paths.php
@@ -11,8 +11,14 @@ desc('Cleaning up files and/or directories');
 task('deploy:clear_paths', function () {
     $paths = get('clear_paths');
     $sudo = get('clear_use_sudo') ? 'sudo' : '';
+    $batch = 100;
 
+    $commands = [];
     foreach ($paths as $path) {
-        run("$sudo rm -rf {{release_path}}/$path");
+        $commands[] = "$sudo rm -rf {{release_path}}/$path";
+    }
+    $chunks = array_chunk($commands, $batch);
+    foreach ($chunks as $chunk) {
+        run(implode('; ', $chunk));
     }
 });

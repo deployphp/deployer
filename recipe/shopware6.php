5f5137d6cc404ded8d7e8225e28a5f8e600e2871
Shyim <6224096+shyim@users.noreply.github.com>
2020-06-15 08:54:13 +0200

    Shopware 6, all shared folders

--- recipe/shopware6.php
+++ recipe/shopware6.php
@@ -7,32 +7,45 @@ set('shared_files', [
     '.env'
 ]);
 set('shared_dirs', [
-    'var',
+    'custom/plugins',
+    'config/jwt',
+    'files',
+    'var/log',
     'public/media',
-    'public/thumbnail'
+    'public/thumbnail',
+    'public/sitemap',
 ]);
 set('writable_dirs', [
-    'var',
+    'custom/plugins',
+    'config/jwt',
+    'files',
+    'var/log',
     'public/media',
-    'public/thumbnail'
+    'public/thumbnail',
+    'public/sitemap'
 ]);
 set('static_folders', []);
 set('writable_dirs', [
     'public'
 ]);
 
-task('sw:update_code', function(){
+task('sw:update_code', function() {
     run('git clone {{repository}} {{release_path}};');
 });
 task('sw:deploy','
     cd {{release_path}};
     composer install;
-    ');
+');
 
-task('sw:theme:compile', function(){
+task('assets:install', function() {
+    run('cd {{release_path}} && bin/console assets:install;');
+});
+
+task('theme:compile', function() {
     run('cd {{release_path}} && bin/console theme:compile;');
 });
-task('sw:cache:clear', function(){
+
+task('cache:clear', function() {
     run('cd {{release_path}} && bin/console cache:clear;');
 });
 /**
@@ -46,8 +59,9 @@ task('deploy', [
     'sw:deploy',
     'deploy:shared',
     'deploy:writable',
-    'sw:theme:compile',
-    'sw:cache:clear',
+    'assets:install',
+    'theme:compile',
+    'cache:clear',
     'deploy:clear_paths',
     'deploy:symlink',
     'deploy:unlock',

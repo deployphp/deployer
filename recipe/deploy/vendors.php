cdb5a169a9680c4c6b415b901e6d425dfe40a58d
Rico Neitzel <rico@buro71a.de>
2020-09-04 03:17:28 +0200

    fixes composer not using specified {{bin/php}}

--- recipe/deploy/vendors.php
+++ recipe/deploy/vendors.php
@@ -12,5 +12,5 @@ task('deploy:vendors', function () {
     if (!commandExist('unzip')) {
         warning('To speed up composer installation setup "unzip" command with PHP zip extension.');
     }
-    run('cd {{release_path}} && {{bin/composer}} {{composer_options}} 2>&1');
+    run('cd {{release_path}} && {{bin/php}} {{bin/composer}} {{composer_options}} 2>&1');
 });

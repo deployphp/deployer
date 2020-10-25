ca2e42a3b591b9fb4ef9ab81af4b5d04b8b46ff7
Anton Medvedev <anton@medv.io>
2020-10-25 14:49:54 +0100

    Add docgen check action

--- recipe/deploy/vendors.php
+++ recipe/deploy/vendors.php
@@ -14,6 +14,7 @@ set('composer_options', '--verbose --prefer-dist --no-progress --no-interaction 
  * ```php
  *     set('composer_version', '10.10.15')
  * ```
+ *
  */
 set('composer_version', null);
 

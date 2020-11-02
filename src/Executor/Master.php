14b97b970c8ce29141e47b7f8e78f74767207b91
Markus Staab <47448731+clxmstaab@users.noreply.github.com>
2020-11-02 10:03:25 +0100

    Restore Master.php

--- src/Executor/Master.php
+++ src/Executor/Master.php
@@ -274,7 +274,6 @@ class Master
 
     /**
      * @param Process[] $processes
-     * @phpstan-param callable(string):void $callback
      */
     protected function gatherOutput(array $processes, callable $callback): void
     {

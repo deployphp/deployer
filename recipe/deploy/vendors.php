1cad2da445863dd5084783b07a2ec405da7f49c4
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2020-12-11 19:28:52 +0100

    Refresh composer version according to 'composer_version' or 'composer_channel'. Add comments.

--- recipe/deploy/vendors.php
+++ recipe/deploy/vendors.php
@@ -36,17 +36,24 @@ set('bin/composer', function () {
     }
 
     if ($composerBin) {
+        // "composer --version" can return:
+        // - "Composer version 1.10.17 2020-10-30 22:31:58" - for stable channel
+        // - "Composer version 2.0.0-alpha3 2020-10-30 22:31:58" - alpha/beta/RC for preview channel
+        // - "Composer version 2.0-dev (2.0-dev+378a5b72b9f81e8e919e41ecd3add6893d14b90e) 2020-12-04 09:50:19" - for snapshot channel
         $currentComposerVersionRaw = run($composerBin . ' --version');
         if (preg_match('/(\\d+\\.\\d+)(\\.\\d+)?-?(RC|alpha|beta|dev)?\d*/', $currentComposerVersionRaw, $composerVersionMatches)) {
             $currentComposerVersion = $composerVersionMatches[0] ?? null;
             if ($currentComposerVersion) {
+                // if we have exact version of composer to install (composer_version) and currently installed version match it then return
                 if ($composerVersionToInstall && $currentComposerVersion === (string)$composerVersionToInstall) {
                     return $composerBin;
                 }
                 if ($composerChannelToInstall && !$composerVersionToInstall) {
+                    // for snapshot channel the version is the git hash in "Composer version 2.0-dev (2.0-dev+378a5b72b9f81e8e919e41ecd3add6893d14b90e) 2020-12-04 09:50:19"
                     if (preg_match('/\\+(.*)\\)/', $currentComposerVersionRaw, $snapshotVersion)) {
                         $currentComposerVersion = $snapshotVersion[1] ?? null;
                     }
+                    // Compare latest version of composer channel with and currently installed version. If match then return.
                     $composerChannelData = json_decode(file_get_contents('https://getcomposer.org/versions'), true);
                     $latestComposerVersionFromChannel = $composerChannelData[$composerChannelToInstall][0]['version'] ?? null;
                     if ($latestComposerVersionFromChannel && $latestComposerVersionFromChannel === $currentComposerVersion) {

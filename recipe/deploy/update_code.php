9f6e9d61613b727f50bfb2aee4bd85c0324b85c4
Anton Medvedev <anton@medv.io>
2021-03-14 23:20:01 +0100

    Refactor deploy:update_code

--- recipe/deploy/update_code.php
+++ recipe/deploy/update_code.php
@@ -1,8 +1,6 @@
 <?php
 namespace Deployer;
 
-use Deployer\Exception\RunException;
-
 /**
  * Determines which branch to deploy. Can be overridden with cli option `--branch`.
  * If not specified, will get current git HEAD branch as default branch to deploy.
@@ -10,12 +8,8 @@ use Deployer\Exception\RunException;
 set('branch', function () {
     try {
         $branch = runLocally('git rev-parse --abbrev-ref HEAD');
-    } catch (\Throwable $exception) {
+    } catch (\Throwable) {
         $branch = null;
-    }
-
-    if ($branch === 'HEAD') {
-        $branch = null; // Travis-CI fix
     }
 
     if (input()->hasOption('branch') && !empty(input()->getOption('branch'))) {
@@ -26,25 +20,6 @@ set('branch', function () {
 });
 
 /**
- * This config option will if set to true will instructs git to use previous release files,
- * and download only changed files from server.
- *
- * You don't need to set this option, it will automatically detect if your git supports this feature.
- *
- * Faster cloning by borrowing objects from existing clones.
- */
-set('git_cache', function () {
-    $gitVersion = run('{{bin/git}} version');
-    $regs = [];
-    if (preg_match('/((\d+\.?)+)/', $gitVersion, $regs)) {
-        $version = $regs[1];
-    } else {
-        $version = "1.0.0";
-    }
-    return version_compare($version, '2.3', '>=');
-});
-
-/**
  * Update code at {{release_path}} on host.
  */
 desc('Update code');
@@ -52,62 +27,42 @@ task('deploy:update_code', function () {
     $repository = get('repository');
     $branch = get('branch');
     $git = get('bin/git');
-    $gitCache = get('git_cache');
-    $recursive = get('git_recursive', true) ? '--recursive' : '';
-    $dissociate = get('git_clone_dissociate', true) ? '--dissociate' : '';
-    $quiet = output()->isQuiet() ? '-q' : '';
-    $depth = $gitCache ? '' : '--depth 1';
 
     $at = '';
     if (!empty($branch)) {
-        $at = "-b \"$branch\"";
+        $at = $branch;
     }
 
-    // If option `tag` is set
+    // If option `tag` is set.
     if (input()->hasOption('tag')) {
         $tag = input()->getOption('tag');
         if (!empty($tag)) {
-            $at = "-b \"$tag\"";
+            $at = $tag;
         }
     }
 
-    // If option `tag` is not set and option `revision` is set
-    if (empty($tag) && input()->hasOption('revision')) {
+    // If option `tag` is not set and option `revision` is set.
+    if (empty($tag)) {
         $revision = input()->getOption('revision');
         if (!empty($revision)) {
-            $depth = '';
+            $at = $revision;
         }
     }
 
-    // Enter deploy_path if present
-    if (has('deploy_path')) {
-        cd('{{deploy_path}}');
-    }
+    $bare = parse('{{deploy_path}}/.dep/repo');
+    run("[ -d $bare ] || mkdir -p $bare");
+    run("[ -f $bare/HEAD ] || $git clone --mirror $repository $bare 2>&1");
 
-    // Populate known hosts
-    preg_match('/.*(@|\/\/)([^\/:]+).*/', $repository, $match);
-    if (isset($match[2])) {
-        $repositoryHostname = $match[2];
-        try {
-            run("ssh-keygen -F $repositoryHostname");
-        } catch (RunException $exception) {
-            run("ssh-keyscan -H $repositoryHostname >> ~/.ssh/known_hosts");
-        }
-    }
+    cd($bare);
 
-    if ($gitCache && has('previous_release')) {
-        try {
-            run("$git clone $at $recursive $quiet --reference {{previous_release}} $dissociate $repository  {{release_path}} 2>&1");
-        } catch (\Throwable $exception) {
-            // If {{deploy_path}}/releases/{$releases[1]} has a failed git clone, is empty, shallow etc, git would throw error and give up. So we're forcing it to act without reference in this situation
-            run("$git clone $at $recursive $quiet $repository {{release_path}} 2>&1");
-        }
-    } else {
-        // if we're using git cache this would be identical to above code in catch - full clone. If not, it would create shallow clone.
-        run("$git clone $at $depth $recursive $quiet $repository {{release_path}} 2>&1");
-    }
+    // If remote url change, update in .git/repo as well.
+    run("[[ $($git remote get-url origin) == '$repository' ]] || $git remote set-url origin $repository ");
 
-    if (!empty($revision)) {
-        run("cd {{release_path}} && $git checkout $revision");
-    }
+    // Copy to release_path.
+    run("$git remote update 2>&1");
+    run("$git archive $at | tar -x -f - -C {{release_path}} 2>&1");
+
+    // Save revision in releases log.
+    $rev = run("$git rev-list $at -1");
+    run("sed -i 's/revision/$rev/' {{deploy_path}}/.dep/releases");
 });

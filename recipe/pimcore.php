af35c3b98b8b678fdacf0bedbafb4083fdf2ae7c
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2021-08-23 13:57:54 +0200

    update pimcore recipe to include new tasks into deploy automatically

--- recipe/pimcore.php
+++ recipe/pimcore.php
@@ -11,11 +11,18 @@ add('shared_dirs', ['public/var', 'var/email', 'var/recyclebin', 'var/versions']
 add('writable_dirs', ['public/var']);
 
 desc('Rebuild Pimcore Classes');
-task('deploy:pimcore:rebuild-classes', function () {
-    run('{{bin/php}} {{bin/console}} pimcore:deployment:classes-rebuild -c -d -n');
+task('pimcore:rebuild-classes', function () {
+    run('{{bin/php}} {{bin/console}} pimcore:deployment:classes-rebuild --create --delete --no-interaction');
 });
 
 desc('Create Custom Layouts');
-task('deploy:pimcore:custom-layouts-rebuild', function () {
-    run('{{bin/php}} {{bin/console}} pimcore:deployment:custom-layouts-rebuild -c -d -n');
+task('pimcore:custom-layouts-rebuild', function () {
+    run('{{bin/php}} {{bin/console}} pimcore:deployment:custom-layouts-rebuild --create --delete --no-interaction');
 });
+
+task('pimcore:deploy', [
+    'pimcore:rebuild-classes',
+    'pimcore:custom-layouts-rebuild'
+]);
+
+after('deploy:vendors', 'pimcore:deploy');

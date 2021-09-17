76bfd230054abf6ab52175ddb2a8ce608455d042
gjuric <gjuric@gmail.com>
2021-09-17 16:34:35 +0200

    Throw an exception if copy_dirs entries end with a slash

--- recipe/deploy/copy_dirs.php
+++ recipe/deploy/copy_dirs.php
@@ -5,6 +5,9 @@ desc('Copy directories');
 task('deploy:copy_dirs', function () {
     if (has('previous_release')) {
         foreach (get('copy_dirs') as $dir) {
+            if (substr($dir, -1) === '/') {
+                throw new \RuntimeException('Entries in config parameter "copy_dirs" must not end with "/"');
+            }
             if (test("[ -d {{previous_release}}/$dir ]")) {
                 run("mkdir -p {{release_path}}/$dir");
                 run("cp -rpf {{previous_release}}/$dir {{release_path}}/$dir");

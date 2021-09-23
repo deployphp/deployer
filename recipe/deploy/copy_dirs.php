34003df6f6b8c534e64cf56d0ffa507a34a9f1c4
gjuric <gjuric@gmail.com>
2021-09-23 18:39:43 +0200

    Fix to strip last directory name from the destination folder

--- recipe/deploy/copy_dirs.php
+++ recipe/deploy/copy_dirs.php
@@ -9,8 +9,12 @@ task('deploy:copy_dirs', function () {
                 throw new \RuntimeException('Entries in config parameter "copy_dirs" must not end with "/"');
             }
             if (test("[ -d {{previous_release}}/$dir ]")) {
+                $destinationDir = '';
+                if (strpos($dir, '/') !== false) {
+                    $destinationDir = substr($dir, 0, strrpos($dir, '/') + 1);
+                }
                 run("mkdir -p {{release_path}}/$dir");
-                run("cp -rpf {{previous_release}}/$dir {{release_path}}/$dir");
+                run("cp -rpf {{previous_release}}/$dir {{release_path}}/$destinationDir");
             }
         }
     }

245ae7f7c58585d99b42731568ea3576608fc8b2
Anton Medvedev <anton@medv.io>
2021-10-11 22:46:02 +0200

    Automatically create update banner

--- src/Support/Config/version.php
+++ src/Support/Config/version.php
@@ -1,9 +1,21 @@
 <?php declare(strict_types=1);
 namespace Deployer;
 
+set('banner', <<<EOF
+╭──────────────────────────────────────────────────────╮
+│                                                      │
+│   Update available! https://deployer.org/download/   │
+│                                                      │
+╰──────────────────────────────────────────────────────╯
+EOF
+);
+
+set('old_version', function (): string {
+    return explode(' ', run("bin/dep --version"))[1];
+});
+
 set('version', function (): string {
     cd('{{depl}}');
-    $version = explode(' ', run("bin/dep --version"))[1];
-    writeln("Current version is $version");
+    writeln("Current version is {{old_version}}");
     return ask('Type new version (format is "1.1.1", without "v"; pre release "1.1.1-beta")', '--patch');
 });

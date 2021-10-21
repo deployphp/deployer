83a3c369a79e7ae85a26e1c239beb6a1ad329e82
Anton Medvedev <anton@medv.io>
2021-10-21 23:19:44 +0200

    ==>

--- src/Support/update_manifest.php
+++ src/Support/update_manifest.php
@@ -1,4 +1,10 @@
-<?php
+<?php declare(strict_types=1);
+/* (c) Anton Medvedev <anton@medv.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Deployer;
 
 task('update_manifest', function () {

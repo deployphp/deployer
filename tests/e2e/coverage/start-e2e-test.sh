c37e3c04c4d48a2f4dd1798c41d65b02f8c34e07
Rigel Kent Carbonel <rigel20.kent@gmail.com>
2022-01-29 00:05:25 +0800

    Remove php 7.3 Update the start-e2e-test.sh to add XDEBUG_MODE

--- tests/e2e/coverage/start-e2e-test.sh
+++ tests/e2e/coverage/start-e2e-test.sh
@@ -3,7 +3,7 @@
 ROOTDIR=$(readlink -f "$(dirname "$0")/../../..")
 
 # Run E2E tests and grab exit code of the process
-php "$ROOTDIR/vendor/bin/pest" --config "$ROOTDIR/tests/e2e/phpunit-e2e.xml"
+php XDEBUG_MODE=coverage "$ROOTDIR/vendor/bin/pest" --config "$ROOTDIR/tests/e2e/phpunit-e2e.xml"
 E2E_EXIT_CODE=$?
 
 # Generate coverage report file

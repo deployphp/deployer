6bbaef77593b40b0d32f6939fab7ddbd5d8b2756
Rigel Kent Carbonel <rigel20.kent@gmail.com>
2022-01-30 11:37:24 +0800

    Added type hints and removed useless phpdoc for phpcs.

--- src/Component/Pimple/Container.php
+++ src/Component/Pimple/Container.php
@@ -52,13 +52,9 @@ class Container implements \ArrayAccess
      * as function names (strings) are callable (creating a function with
      * the same name as an existing parameter would break your container).
      *
-     * @param string $id    The unique identifier for the parameter or object
-     * @param mixed  $value The value of the parameter or a closure to define an object
-     *
      * @throws FrozenServiceException Prevent override of a frozen service
-     * @phpcsSuppress SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingNativeTypeHint
      */
-    public function offsetSet($id, mixed $value): void
+    public function offsetSet(mixed $id, mixed $value): void
     {
         if (isset($this->frozen[$id])) {
             throw new FrozenServiceException($id);
@@ -71,14 +67,9 @@ class Container implements \ArrayAccess
     /**
      * Gets a parameter or an object.
      *
-     * @param string $id The unique identifier for the parameter or object
-     *
-     * @return mixed The value of the parameter or an object
-     *
      * @throws UnknownIdentifierException If the identifier is not defined
-     * @phpcsSuppress SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingNativeTypeHint
      */
-    public function offsetGet($id): mixed
+    public function offsetGet(mixed $id): mixed
     {
         if (!isset($this->keys[$id])) {
             throw new UnknownIdentifierException($id);
@@ -106,26 +97,12 @@ class Container implements \ArrayAccess
         return $val;
     }
 
-    /**
-     * Checks if a parameter or an object is set.
-     *
-     * @param string $id The unique identifier for the parameter or object
-     *
-     * @return bool
-     * @phpcsSuppress SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingNativeTypeHint
-     */
-    public function offsetExists($id): bool
+    public function offsetExists(mixed $id): bool
     {
         return isset($this->keys[$id]);
     }
 
-    /**
-     * Unsets a parameter or an object.
-     *
-     * @param string $id The unique identifier for the parameter or object
-     * @phpcsSuppress SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingNativeTypeHint
-     */
-    public function offsetUnset($id): void
+    public function offsetUnset(mixed $id): void
     {
         if (isset($this->keys[$id])) {
             if (\is_object($this->values[$id])) {
--- src/Configuration/Configuration.php
+++ src/Configuration/Configuration.php
@@ -150,38 +150,21 @@ class Configuration implements \ArrayAccess
         return isset($matches[1]) ? $this->get($matches[1]) : null;
     }
 
-    /**
-     * @param mixed $offset
-     * @return bool
-     */
     public function offsetExists(mixed $offset): bool
     {
         return $this->has($offset);
     }
 
-    /**
-     * @param string $offset
-     * @return mixed|null
-     * @phpcsSuppress SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingNativeTypeHint
-     */
-    public function offsetGet($offset): mixed
+    public function offsetGet(mixed $offset): mixed
     {
         return $this->get($offset);
     }
 
-    /**
-     * @param string $offset
-     * @param mixed $value
-     * @phpcsSuppress SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingNativeTypeHint
-     */
-    public function offsetSet($offset, mixed $value): void
+    public function offsetSet(mixed $offset, mixed $value): void
     {
         $this->set($offset, $value);
     }
 
-    /**
-     * @param mixed $offset
-     */
     public function offsetUnset(mixed $offset): void
     {
         unset($this->values[$offset]);

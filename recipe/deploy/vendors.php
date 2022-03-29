e58287be180721cefe894709cf995c01f5d438fb
Nicolas Lemoine <nico.lemoine@gmail.com>
2022-03-29 21:27:36 +0200

    - Allow multiple vendors directories - Automatically run self update composer every N days

--- recipe/deploy/vendors.php
+++ recipe/deploy/vendors.php
@@ -1,15 +1,30 @@
 <?php
 namespace Deployer;
 
+set('vendor_paths', ['{{release_or_current_path}}']);
 set('composer_action', 'install');
+
+// Self update composer from time to time
+// Set `false` to disable or an integer to update every N days
+// Default to 30 days
+set('composer_self_update', 30);
 
 set('composer_options', '--verbose --prefer-dist --no-progress --no-interaction --no-dev --optimize-autoloader');
 
-// Returns Composer binary path in found. Otherwise try to install latest
+// Returns Composer binary path if found. Otherwise try to install latest
 // composer version to `.dep/composer.phar`. To use specific composer version
 // download desired phar and place it at `.dep/composer.phar`.
 set('bin/composer', function () {
     if (test('[ -f {{deploy_path}}/.dep/composer.phar ]')) {
+        // If composer.phar is older than `composer_self_update` days, run self update
+        if(
+            get('composer_self_update', 0)
+            && strtotime(sprintf('+%d days', (int) get('composer_self_update')), (int) run('stat -c %Y {{deploy_path}}/.dep/composer.phar')) <= time()
+        ) {
+            warning("Composer is older than {{composer_self_update}} days, updating...");
+            run('{{deploy_path}}/.dep/composer.phar self-update');
+        }
+
         return '{{bin/php}} {{deploy_path}}/.dep/composer.phar';
     }
 
@@ -28,5 +43,7 @@ task('deploy:vendors', function () {
     if (!commandExist('unzip')) {
         warning('To speed up composer installation setup "unzip" command with PHP zip extension.');
     }
-    run('cd {{release_or_current_path}} && {{bin/composer}} {{composer_action}} {{composer_options}} 2>&1');
+    foreach (get('vendor_paths', []) as $path) {
+        run("cd $path && {{bin/composer}} {{composer_action}} {{composer_options}} 2>&1");
+    }
 });

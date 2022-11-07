7920f8b206695313589c923c15b0a2a9cb69460b
Sander De la Marche <sander@sumocoders.be>
2022-11-07 10:58:29 +0100

    Select the appropriate cachetool url based on the PHP version

--- contrib/cachetool.php
+++ contrib/cachetool.php
@@ -51,7 +51,13 @@ http://gordalina.github.io/cachetool/
 namespace Deployer;
 
 set('cachetool', '');
-set('cachetool_url', 'https://github.com/gordalina/cachetool/releases/download/7.0.0/cachetool.phar');
+set('cachetool_url', function () {
+    if (PHP_VERSION_ID < 81000) {
+        return 'https://github.com/gordalina/cachetool/releases/download/8.0.0/cachetool.phar';
+    }
+
+    return 'https://github.com/gordalina/cachetool/releases/download/9.0.0/cachetool.phar';
+});
 set('cachetool_args', '');
 set('bin/cachetool', function () {
     if (!test('[ -f {{release_or_current_path}}/cachetool.phar ]')) {

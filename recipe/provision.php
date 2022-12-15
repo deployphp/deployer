890606b724166cbfcb329e633d252727a9c1dc7b
Thomas Navarro <36193187+thomasnavarro@users.noreply.github.com>
2022-12-15 15:03:27 +0100

    fix #3321 isssue
    Adding --yes flag to Caddy gpg command

--- recipe/provision.php
+++ recipe/provision.php
@@ -84,7 +84,7 @@ task('provision:update', function () {
     run('apt-add-repository ppa:ondrej/php -y', ['env' => ['DEBIAN_FRONTEND' => 'noninteractive']]);
 
     // Caddy
-    run("curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg");
+    run("curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor --yes -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg");
     run("curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' > /etc/apt/sources.list.d/caddy-stable.list");
 
     // Nodejs

aeec635a2dcdf8817958dfff245f96834c289837
Daniel Hormess <dani.hormess@icloud.com>
2023-02-24 18:22:55 +0100

    Fixed bin/console calls in shopware recipe (#3512)
    * Fixed bin/console calls in shopware recipe
    
    * Added generated docs
    
    ---------
    
    Co-authored-by: Daniel Hormess <daniel@proudcommerce.com>

--- recipe/shopware.php
+++ recipe/shopware.php
@@ -19,6 +19,7 @@ require_once __DIR__ . '/common.php';
 
 add('recipes', ['shopware']);
 
+set('bin/console', '{{bin/php}} {{release_or_current_path}}/bin/console');
 
 set('default_timeout', 3600); // Increase when tasks take longer than that.
 
@@ -59,28 +60,28 @@ set('writable_dirs', [
 
 // This task remotely executes the `cache:clear` console command on the target server.
 task('sw:cache:clear', static function () {
-    run('cd {{release_path}} && bin/console cache:clear');
+    run('cd {{release_path}} && {{bin/console}} cache:clear');
 });
 
 // This task remotely executes the cache warmup console commands on the target server, so that the first user, who
 // visits the website, doesn't have to wait for the cache to be built up.
 task('sw:cache:warmup', static function () {
-    run('cd {{release_path}} && bin/console cache:warmup');
-    run('cd {{release_path}} && bin/console http:cache:warm:up');
+    run('cd {{release_path}} && {{bin/console}} cache:warmup');
+    run('cd {{release_path}} && {{bin/console}} http:cache:warm:up');
 });
 
 // This task remotely executes the `database:migrate` console command on the target server.
 task('sw:database:migrate', static function () {
-    run('cd {{release_path}} && bin/console database:migrate --all');
+    run('cd {{release_path}} && {{bin/console}} database:migrate --all');
 });
 
 task('sw:plugin:refresh', function () {
-    run('cd {{release_path}} && bin/console plugin:refresh');
+    run('cd {{release_path}} && {{bin/console}} plugin:refresh');
 });
 
 function getPlugins(): array
 {
-    $output = explode("\n", run('cd {{release_path}} && bin/console plugin:list'));
+    $output = explode("\n", run('cd {{release_path}} && {{bin/console}} plugin:list'));
 
     // Take line over headlines and count "-" to get the size of the cells.
     $lengths = array_filter(array_map('strlen', explode(' ', $output[4])));
@@ -115,7 +116,7 @@ task('sw:plugin:update:all', static function () {
     foreach ($plugins as $plugin) {
         if ($plugin['Installed'] === 'Yes') {
             writeln("<info>Running plugin update for " . $plugin['Plugin'] . "</info>\n");
-            run("cd {{release_path}} && bin/console plugin:update " . $plugin['Plugin']);
+            run("cd {{release_path}} && {{bin/console}} plugin:update " . $plugin['Plugin']);
         }
     }
 });
@@ -151,10 +152,10 @@ task('sw-build-without-db:get-remote-config', static function () {
         return;
     }
     within('{{deploy_path}}/current', function () {
-        run('./bin/console bundle:dump');
+        run('{{bin/php}} ./bin/console bundle:dump');
         download('{{deploy_path}}/current/var/plugins.json', './var/');
 
-        run('./bin/console theme:dump');
+        run('{{bin/php}} ./bin/console theme:dump');
         download('{{deploy_path}}/current/files/theme-config', './files/');
     });
 });

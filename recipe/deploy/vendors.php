7a82df273faa5ddf96d9f08fcb9ab6796aa6b198
Ernst Jendritzki <ernstjendritzki@web.de>
2023-04-06 22:43:17 +0200

    Also install composer if outdated

--- recipe/deploy/vendors.php
+++ recipe/deploy/vendors.php
@@ -1,23 +1,28 @@
 <?php
+
 namespace Deployer;
 
 set('composer_action', 'install');
 
 set('composer_options', '--verbose --prefer-dist --no-progress --no-interaction --no-dev --optimize-autoloader');
 
-// Returns Composer binary path in found. Otherwise try to install latest
-// composer version to `.dep/composer.phar`. To use specific composer version
-// download desired phar and place it at `.dep/composer.phar`.
+// If Composer is installed and version >= 2.0.0.: Returns Composer binary path in found.
+// Otherwise try to install latest composer version to `.dep/composer.phar`.
+// To use specific composer version, download desired phar and place it at `.dep/composer.phar`.
 set('bin/composer', function () {
     if (test('[ -f {{deploy_path}}/.dep/composer.phar ]')) {
-        return '{{bin/php}} {{deploy_path}}/.dep/composer.phar';
+        $composerVersion = run('{{bin/php}} {{deploy_path}}/.dep/composer.phar --version');
+        if (version_compare($composerVersion, '2.0.0', '>=')) {
+            return '{{bin/php}} {{deploy_path}}/.dep/composer.phar';
+        }
     }
 
-    if (commandExist('composer')) {
+    $globalComposerVersion = run('composer --version --no-ansi --no-interaction');
+    if (version_compare($globalComposerVersion, '2.0.0', '>=')) {
         return '{{bin/php}} ' . which('composer');
     }
 
-    warning("Composer binary wasn't found. Installing latest composer to \"{{deploy_path}}/.dep/composer.phar\".");
+    warning("Composer binary wasn't found or is outdated. Installing latest composer to \"{{deploy_path}}/.dep/composer.phar\".");
     run("cd {{deploy_path}} && curl -sS https://getcomposer.org/installer | {{bin/php}}");
     run('mv {{deploy_path}}/composer.phar {{deploy_path}}/.dep/composer.phar');
     return '{{bin/php}} {{deploy_path}}/.dep/composer.phar';

fe002d05715c40b8b68631077c2c78f93e25bf56
Propaganistas <Propaganistas@users.noreply.github.com>
2024-02-26 09:40:42 +0100

    add release name to info

--- recipe/deploy/info.php
+++ recipe/deploy/info.php
@@ -3,5 +3,5 @@ namespace Deployer;
 
 desc('Displays info about deployment');
 task('deploy:info', function () {
-    info("deploying <fg=magenta;options=bold>{{target}}</>");
+    info("deploying <fg=magenta;options=bold>{{target}}</> (release <fg=magenta;options=bold>{{release_name}}</>)");
 });

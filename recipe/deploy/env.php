27188e0fd7177413b756b7aab8f4b5e8b322a7a1
Anton Medvedev <anton@medv.io>
2024-11-08 14:11:50 +0100

    Fix .env file test in deploy:env task

--- recipe/deploy/env.php
+++ recipe/deploy/env.php
@@ -7,7 +7,7 @@ set('dotenv_example', '.env.example');
 desc('Configure .env file');
 task('deploy:env', function () {
     cd('{{release_or_current_path}}');
-    if (test('[ ! -f .env ] && [ -f {{dotenv_example}} ]')) {
+    if (test('[ ! -e .env ] && [ -f {{dotenv_example}} ]')) {
         run('cp {{dotenv_example}} .env');
         set('new_deployment', true);
     }

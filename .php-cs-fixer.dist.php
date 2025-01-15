fbe18e49d1578391033be90efa401719a6d33d6a
Luca Castellone <luca.castellone@gmail.com>
2025-01-15 20:53:31 +0100

    Fix php7.4 after 7.5.6

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -9,5 +9,6 @@ $finder = (new PhpCsFixer\Finder())
 return (new PhpCsFixer\Config())
     ->setRules([
         '@PER-CS' => true,
+        'trailing_comma_in_multiline' => ['elements' => ['arguments', 'array_destructuring', 'arrays']], // For PHP 7.4 compatibility
     ])
     ->setFinder($finder);

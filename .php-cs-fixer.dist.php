e25bedc78b5ed73a38c5351f06fb72227dd33948
Anton Medvedev <anton@medv.io>
2025-02-10 14:20:58 +0100

    wip

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -9,6 +9,14 @@ $finder = (new PhpCsFixer\Finder())
 return (new PhpCsFixer\Config())
     ->setRules([
         '@PER-CS' => true,
-        'trailing_comma_in_multiline' => ['elements' => ['arguments', 'array_destructuring', 'arrays']], // For PHP 7.4 compatibility
+
+        // Due to historical reasons we have to keep this.
+        // Docs parser expects comment right after php tag.
+        'blank_line_after_opening_tag' => false,
+
+        // For PHP 7.4 compatibility.
+        'trailing_comma_in_multiline' => [
+            'elements' => ['arguments', 'array_destructuring', 'arrays']
+        ],
     ])
     ->setFinder($finder);

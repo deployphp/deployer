70df6ff12d133e1ade54bc9e6c45cee955415da4
David Bonhagen <david.bonhagen@fullhaus.de>
2025-07-11 10:15:22 +0200

    Update typo3.php (#4094)
    * Update typo3.php
    
    * Add regenerated docs
    
    * refactor: remove tasks to remove
    
    * refactor: wrap composer in set config option
    
    * refactor: enable user to choose transfer method
    
    * chore: regenerated documentation
    
    * fix: apply php-cs-fixer
    
    * refactor: use absolute path to execute typo3 binary
    
    * refactor: remove effectless warm up
    
    * refactor: rename public dir and adjust shared / writable dirs
    
    * refactor: rename rsync task for more consistency
    
    * refactor: adjust exclude list
    
    * refactor: make shared files configurable
    
    * refactor: extend cache flush
    
    * refactor: reduce shared files
    
    * fix: revert rsync task renaming

--- recipe/typo3.php
+++ recipe/typo3.php
@@ -3,46 +3,172 @@
 namespace Deployer;
 
 require_once __DIR__ . '/common.php';
+require_once 'contrib/rsync.php';
 
 add('recipes', ['typo3']);
+
+set('composer_config', function () {
+    return json_decode(file_get_contents('./composer.json'), true, 512, JSON_THROW_ON_ERROR);
+});
 
 /**
  * DocumentRoot / WebRoot for the TYPO3 installation
  */
-set('typo3_webroot', 'Web');
+set('typo3/public_dir', function () {
+    $composerConfig = get('composer_config');
+
+    if ($composerConfig['extra']['typo3/cms']['web-dir'] ?? false) {
+        return $composerConfig['extra']['typo3/cms']['web-dir'];
+    }
+
+    return 'public';
+});
 
 /**
- * Main TYPO3 task
+ * Path to TYPO3 cli
  */
-desc('Deploys your project');
-task('deploy', [
-    'deploy:prepare',
-    'deploy:vendors',
-    'deploy:publish',
-]);
+set('bin/typo3', function () {
+    $composerConfig = get('composer_config');
+
+    if ($composerConfig['config']['bin-dir'] ?? false) {
+        return $composerConfig['config']['bin-dir'] . '/typo3';
+    }
+
+    return 'vendor/bin/typo3';
+});
+
+/**
+ * Log files to display when running `./vendor/bin/dep logs:app`
+ */
+set('log_files', 'var/log/typo3_*.log');
 
 /**
  * Shared directories
  */
 set('shared_dirs', [
-    '{{typo3_webroot}}/fileadmin',
-    '{{typo3_webroot}}/typo3temp',
-    '{{typo3_webroot}}/uploads',
+    '{{typo3/public_dir}}/fileadmin',
+    '{{typo3/public_dir}}/assets',
+    '{{typo3/public_dir}}/typo3temp/assets',
+    'var/lock',
+    'var/log',
+    'var/session',
+    'var/spool',
 ]);
 
 /**
  * Shared files
  */
-set('shared_files', [
-    '{{typo3_webroot}}/.htaccess',
-]);
+if (!has('shared_files') || empty(get('shared_files'))) {
+    set('shared_files', [
+        'config/system/settings.php',
+    ]);
+}
 
 /**
  * Writeable directories
  */
 set('writable_dirs', [
-    '{{typo3_webroot}}/fileadmin',
-    '{{typo3_webroot}}/typo3temp',
-    '{{typo3_webroot}}/typo3conf',
-    '{{typo3_webroot}}/uploads',
+    '{{typo3/public_dir}}/fileadmin',
+    '{{typo3/public_dir}}/assets',
+    '{{typo3/public_dir}}/typo3temp/assets',
+    'var/cache',
+    'var/lock',
+    'var/log',
 ]);
+
+/**
+ * Composer options
+ */
+set('composer_options', ' --no-dev --verbose --prefer-dist --no-progress --no-interaction --optimize-autoloader');
+
+
+/**
+ * If set in the config this recipe uses rsync. Default: false (use the Git repository)
+ */
+set('use_rsync', false);
+
+set('update_code_task', function () {
+    return get('use_rsync') ? 'rsync' : 'deploy:update_code';
+});
+
+task('typo3:update_code', function () {
+    invoke(get('update_code_task'));
+});
+
+$exclude = [
+    '.Build',
+    '.git',
+    '.gitlab',
+    '.ddev',
+    '.deployer',
+    '.idea',
+    '.DS_Store',
+    '.gitlab-ci.yml',
+    '.npm',
+    'deploy.yaml',
+    'package.json',
+    'package-lock.json',
+    'node_modules/',
+    'var/',
+    '/{{typo3/public_dir}}/assets',
+    '/{{typo3/public_dir}}/fileadmin',
+    '/{{typo3/public_dir}}/typo3temp',
+];
+
+set('rsync', [
+    'exclude' => array_merge(get('shared_dirs'), get('shared_files'), $exclude),
+    'exclude-file' => false,
+    'include' => ['vendor'],
+    'include-file' => false,
+    'filter' => ['dir-merge,-n /.gitignore'],
+    'filter-file' => false,
+    'filter-perdir' => false,
+    'flags' => 'avz',
+    'options' => ['delete', 'keep-dirlinks', 'links'],
+    'timeout' => 600,
+]);
+
+
+desc('TYPO3 - Clear all caches');
+task('typo3:cache:flush', function () {
+    run('{{bin/php}} {{release_path}}/public/typo3 cache:flush ');
+});
+
+desc('TYPO3 - Cache warmup for system caches');
+task('typo3:cache:warmup', function () {
+    run('{{bin/php}} {{release_path}}/public/typo3 cache:warmup --group system');
+});
+
+desc('TYPO3 - Update the language files of all activated extensions');
+task('typo3:language:update', function () {
+    run('{{bin/php}} {{release_path}}/public/typo3 language:update');
+});
+
+desc('TYPO3 - Set up all extensions');
+task('typo3:extension:setup', function () {
+    run('{{bin/php}} {{release_path}}/public/typo3 extension:setup');
+});
+
+/**
+ * Configure "deploy" task group.
+ */
+desc('Deploys a TYPO3 project');
+task('deploy', [
+    'deploy:info',
+    'deploy:setup',
+    'deploy:lock',
+    'deploy:release',
+    'typo3:update_code',
+    'deploy:shared',
+    'deploy:writable',
+    'deploy:vendors',
+    'typo3:cache:warmup',
+    'typo3:extension:setup',
+    'typo3:language:update',
+    'typo3:cache:flush',
+    'deploy:unlock',
+    'deploy:cleanup',
+    'deploy:success',
+]);
+
+after('deploy:failed', 'deploy:unlock');

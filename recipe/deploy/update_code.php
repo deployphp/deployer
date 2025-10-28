5ce97d85e5daaa20292505eb266b832d4ce2b657
Caleb White <cdwhite3@pm.me>
2025-10-28 05:04:03 -0500

    feat: add new `local_archive` update code strategy (#4017)
    * feat: add new `local_archive` update code strategy
    
    * chore: use upload function

--- recipe/deploy/update_code.php
+++ recipe/deploy/update_code.php
@@ -43,8 +43,9 @@ set('target', function () {
 
 // Sets deploy:update_code strategy.
 // Can be one of:
-// - archive
-// - clone (if you need the origin repository `.git` dir in your {{release_path}})
+// - local_archive (copies the repository from local machine)
+// - archive (default, fetches the code from the remote repository)
+// - clone (if you need the origin repository `.git` dir in your {{release_path}}, clones from remote repository)
 set('update_code_strategy', 'archive');
 
 // Sets environment variable _GIT_SSH_COMMAND_ for `git clone` command.
@@ -55,7 +56,7 @@ set('git_ssh_command', 'ssh -o StrictHostKeyChecking=accept-new');
 
 /**
  * Specifies a sub directory within the repository to deploy.
- * Works only when [`update_code_strategy`](#update_code_strategy) is set to `archive` (default).
+ * Works only when [`update_code_strategy`](#update_code_strategy) is set to `archive` (default) or `local_archive`.
  *
  * Example:
  *  - set value to `src` if you want to deploy the folder that lives at `/src`.
@@ -70,55 +71,67 @@ set('sub_directory', false);
  */
 desc('Updates code');
 task('deploy:update_code', function () {
-    $git = get('bin/git');
-    $repository = get('repository');
+    $strategy = get('update_code_strategy');
     $target = get('target');
-
-    if (empty($repository)) {
-        throw new ConfigurationException("Missing 'repository' configuration.");
-    }
+    $git = get('bin/git');
 
     $targetWithDir = $target;
     if (!empty(get('sub_directory'))) {
         $targetWithDir .= ':{{sub_directory}}';
     }
 
-    $bare = parse('{{deploy_path}}/.dep/repo');
-    $env = [
-        'GIT_TERMINAL_PROMPT' => '0',
-        'GIT_SSH_COMMAND' => get('git_ssh_command'),
-    ];
+    if ($strategy === 'local_archive') {
+        runLocally("$git archive $targetWithDir -o archive.tar");
+        upload('archive.tar', '{{release_path}}/archive.tar');
+        run("tar -xf {{release_path}}/archive.tar -C {{release_path}}");
+        run("rm {{release_path}}/archive.tar");
+        unlink('archive.tar');
 
-    start:
-    // Clone the repository to a bare repo.
-    run("[ -d $bare ] || mkdir -p $bare");
-    run("[ -f $bare/HEAD ] || $git clone --mirror $repository $bare 2>&1", env: $env);
-
-    cd($bare);
-
-    // If remote url changed, drop `.dep/repo` and reinstall.
-    if (run("$git config --get remote.origin.url") !== $repository) {
-        cd('{{deploy_path}}');
-        run("rm -rf $bare");
-        goto start;
-    }
-
-    run("$git remote update 2>&1", env: $env);
-
-
-    // Copy to release_path.
-    if (get('update_code_strategy') === 'archive') {
-        run("$git archive $targetWithDir | tar -x -f - -C {{release_path}} 2>&1");
-    } elseif (get('update_code_strategy') === 'clone') {
-        cd('{{release_path}}');
-        run("$git clone -l $bare .");
-        run("$git remote set-url origin $repository", env: $env);
-        run("$git checkout --force $target");
+        $rev = escapeshellarg(runLocally("git rev-list $target -1"));
     } else {
-        throw new ConfigurationException(parse("Unknown `update_code_strategy` option: {{update_code_strategy}}."));
+        $repository = get('repository');
+
+        if (empty($repository)) {
+            throw new ConfigurationException("Missing 'repository' configuration.");
+        }
+
+        $bare = parse('{{deploy_path}}/.dep/repo');
+        $env = [
+            'GIT_TERMINAL_PROMPT' => '0',
+            'GIT_SSH_COMMAND' => get('git_ssh_command'),
+        ];
+
+        start:
+        // Clone the repository to a bare repo.
+        run("[ -d $bare ] || mkdir -p $bare");
+        run("[ -f $bare/HEAD ] || $git clone --mirror $repository $bare 2>&1", env: $env);
+
+        cd($bare);
+
+        // If remote url changed, drop `.dep/repo` and reinstall.
+        if (run("$git config --get remote.origin.url") !== $repository) {
+            cd('{{deploy_path}}');
+            run("rm -rf $bare");
+            goto start;
+        }
+
+        run("$git remote update 2>&1", env: $env);
+
+        // Copy to release_path.
+        if ($strategy === 'archive') {
+            run("$git archive $targetWithDir | tar -x -f - -C {{release_path}} 2>&1");
+        } elseif ($strategy === 'clone') {
+            cd('{{release_path}}');
+            run("$git clone -l $bare .");
+            run("$git remote set-url origin $repository", env: $env);
+            run("$git checkout --force $target");
+        } else {
+            throw new ConfigurationException(parse("Unknown `update_code_strategy` option: {{update_code_strategy}}."));
+        }
+
+        $rev = escapeshellarg(run("$git rev-list $target -1"));
     }
 
     // Save git revision in REVISION file.
-    $rev = escapeshellarg(run("$git rev-list $target -1"));
     run("echo $rev > {{release_path}}/REVISION");
 });

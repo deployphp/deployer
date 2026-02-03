3e3ea6c2615cb591632e896081114f807d64b0e2
alexander-nitsche-governikus <161829362+alexander-nitsche-governikus@users.noreply.github.com>
2026-02-03 14:20:34 +0100

    Install specific Composer version (#4159)

--- recipe/deploy/vendors.php
+++ recipe/deploy/vendors.php
@@ -3,23 +3,27 @@
 namespace Deployer;
 
 set('composer_action', 'install');
-
 set('composer_options', '--verbose --prefer-dist --no-progress --no-interaction --no-dev --optimize-autoloader');
+set('composer_version', null);
 
-// Returns Composer binary path in found. Otherwise try to install latest
-// composer version to `.dep/composer.phar`. To use specific composer version
-// download desired phar and place it at `.dep/composer.phar`.
+// Returns Composer binary path if found. Otherwise, tries to install composer to `.dep/composer.phar`.
 set('bin/composer', function () {
     if (test('[ -f {{deploy_path}}/.dep/composer.phar ]')) {
-        return '{{bin/php}} {{deploy_path}}/.dep/composer.phar';
+        if (empty(get('composer_version')) || preg_match(parse('/Composer.*{{composer_version}}/'), run('{{bin/php}} {{deploy_path}}/.dep/composer.phar --version'))) {
+            return '{{bin/php}} {{deploy_path}}/.dep/composer.phar';
+        }
     }
 
     if (commandExist('composer')) {
-        return '{{bin/php}} ' . which('composer');
+        if (empty(get('composer_version')) || preg_match(parse('/Composer.*{{composer_version}}/'), run('{{bin/php}} ' . which('composer') . ' --version'))) {
+            return '{{bin/php}} ' . which('composer');
+        }
     }
 
-    warning("Composer binary wasn't found. Installing latest composer to \"{{deploy_path}}/.dep/composer.phar\".");
-    run("cd {{deploy_path}} && curl -sS https://getcomposer.org/installer | {{bin/php}}");
+    $versionAsName = get('composer_version') ? ' {{composer_version}}' : '';
+    $versionAsOption = get('composer_version') ? ' -- --version={{composer_version}}' : '';
+    warning("Composer{$versionAsName} wasn't found. Installing to \"{{deploy_path}}/.dep/composer.phar\".");
+    run("cd {{deploy_path}} && curl -sS https://getcomposer.org/installer | {{bin/php}}{$versionAsOption}");
     run('mv {{deploy_path}}/composer.phar {{deploy_path}}/.dep/composer.phar');
     return '{{bin/php}} {{deploy_path}}/.dep/composer.phar';
 });
